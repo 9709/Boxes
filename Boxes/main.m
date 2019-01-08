7e8c678793bbd983524a22ada9b1cfd1c63c7f8e
Matthew Chan <mat_t4@hotmail.com>
2019-01-08 12:07:36 -0800

    Completed

--- Boxes/main.m
+++ Boxes/main.m
@@ -15,7 +15,22 @@ int main(int argc, const char * argv[]) {
                                        initWithWidth:10
                                       initWithLength:10];
     
-    NSLog(@"The volume of the Companion Cube is %.2fcm cube.", [companionCube volume]);
+    float finalVolume = [companionCube volume];
+    NSLog(@"The volume of the Companion Cube is %.2fcm cubed.", finalVolume);
     
+    Box *companionCube2 = [[Box alloc] initWithHeight:20
+                                        initWithWidth:20
+                                       initWithLength:30];
+    
+    float finalVolume2 = [companionCube2 volume];
+    NSLog(@"The volume of the Companion Cube 2 is %.2fcm cubed.", finalVolume2);
+    
+    if (finalVolume > finalVolume2) {
+        float fit = finalVolume2/finalVolume;
+        NSLog(@"Companion Cube 2 can fit in Companion Cube %.2f times.", fit);
+    } else {
+        float fit = finalVolume/finalVolume2;
+        NSLog(@"Companion Cube can fit in Companion Cube 2 %.2f times.", fit);
+    }
     
 }
